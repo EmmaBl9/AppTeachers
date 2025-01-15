25dc4b3e226bfa28f2fd85f220f8964e4832359e
EmmaBlanchoud <156648946+EmmaBl9@users.noreply.github.com>
2025-01-15 16:31:06 +0100

    exercices 0 à 6

---
+++ app-teachers/start/routes.ts
@@ -0,0 +1,14 @@
+/*
+|--------------------------------------------------------------------------
+| Le fichier des routes
+|--------------------------------------------------------------------------
+|
+| Le fichier des routes a pour but de définir toutes les routes HTTP.
+|
+*/
+import TeachersController from '#controllers/teachers_controller'
+import router from '@adonisjs/core/services/router'
+router.get('/', [TeachersController, 'index']).as('home')
+
+// Route permettant de voir les détails d'un enseignant
+router.get('/teacher/:id/show', [TeachersController, 'show']).as('teacher.show')
